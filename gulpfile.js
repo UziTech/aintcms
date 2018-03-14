e5f0315789a2eb3c68193d9b7d4992ec6bbf5a0d
Tony Brix <tony@brix.ninja>
2018-03-14 08:48:54 -0500

    update gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,13 @@
 
 var gulp = require('gulp');
 var gulpSwig = require('gulp-swig');
+var babel = require('gulp-babel');
+var sourcemaps = require('gulp-sourcemaps');
+var sass = require('gulp-sass');
 var swig = require('swig');
 var del = require('del');
 
-gulp.task('templates', function() {
+gulp.task('swig', function() {
   var swigOpts = {
     defaults: {
       loader: swig.loaders.fs(__dirname + '/src'),
@@ -17,13 +20,31 @@ gulp.task('templates', function() {
     .pipe(gulp.dest('dist/'));
 });
 
-gulp.task('assets', function() {
-  return gulp.src('src/!(components)/**/!(*.html)')
-    .pipe(gulp.dest('dist/'));
+gulp.task('babel', function () {
+  return gulp.src('src/js/*')
+    .pipe(sourcemaps.init())
+    .pipe(babel({
+      presets: ['env']
+    }))
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('dist/js/'));
+});
+
+gulp.task('sass', function () {
+  return gulp.src('src/css/*')
+    .pipe(sourcemaps.init())
+    .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('dist/css/'));
+});
+
+gulp.task('images', function() {
+  return gulp.src('src/images/*')
+    .pipe(gulp.dest('dist/images/'));
 });
 
 gulp.task('clean', function() {
   return del(['dist']);
 });
 
-gulp.task('build', gulp.series('clean', gulp.parallel('templates', 'assets')));
+gulp.task('build', gulp.series('clean', 'swig', 'babel', 'sass', 'images'));
